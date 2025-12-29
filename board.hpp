8606f345f8087cfc2344fd880bc60ed1767065e0
Dorel Cohen <dorelcohn1@gmail.com>
2025-12-29 12:17:43 +0200

    conect to vs

--- board.hpp
+++ board.hpp
@@ -7,7 +7,7 @@
 constexpr int BOARD_SIZE_UP = 8;   // Number of squares along one side of the chess board
 constexpr int BOARD_SIZE_RIGHT = 8; // Number of squares along the other side of the chess board
 const std::string STARTING_BOARD_STATE = "RNBQKBNRPPPPPPPP################################pppppppprnbqkbnr0"; // Initial board state
-
+ 
 /* Class definition for the chess board */
 class board 
 { 
